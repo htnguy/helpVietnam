8d8959bf10f672a633effddf8843557ec44d8208
Hieu Nguyen <hieunguyen@Hieus-MBP.cinci.rr.com>
2018-09-02 21:35:20 -0400

    first commit: basic layout

---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require("express"),
+	app = express();
+app.set("view engine", "ejs");
+app.use(express.static(__dirname+"/views"));
+app.use(express.static(__dirname+"/public"));
+
+app.get("/", function(req,res){
+	 res.render("index/index");
+});
+app.get("/about", function(req,res){
+	res.render("index/about");
+});
+app.get("/contact", function(req,res){
+	res.render("index/contact");
+});
+app.get("/founder", function(req,res){
+	res.render("index/founder");
+})
+app.get("/help", function(req,res){
+	res.render("index/help");
+});
+app.get("/learnmore", function(req,res){
+	res.render("index/learnmore");
+});
+
+
+app.listen(3000,function(){
+	console.log("The server is online!");
+})
---
+++ public/app.js
@@ -0,0 +1,15 @@
+
+	// image hover 
+	$('#city').hover(function(){
+		// on hover over
+		$(this).attr('src', 'https://upload.wikimedia.org/wikipedia/commons/6/66/Vi%E1%BB%87t_Nam_v%C3%B4_%C4%91%E1%BB%8Bch_AFF_2008.jpg');
+	}, 
+	function(){ // on hover out
+		$(this).attr('src', 'https://images.unsplash.com/photo-1506741485568-47c278a3e70a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c87c50bc4a77fbe4bd9a5605d97d5f0a&auto=format&fit=crop&w=2664&q=80');
+	});
+
+	// adding the active class to the tab that is selected
+
+
+
+
